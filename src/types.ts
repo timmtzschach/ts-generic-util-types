20dc661146c8cfb72282eeda086d9f4469b508d2
ttzschach <contact@timmtzschach.de>
2024-03-10 00:19:28 +0100

    feat(init): add types

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type AllKeys<T> = T extends T ? keyof T : never;
+
+export type UnionObjectsToIntersection<UnionObjs> = {
+    [UnionObjsKeys in AllKeys<UnionObjs>]: Extract<UnionObjs, { [UnionObjs in UnionObjsKeys]: unknown}>[UnionObjsKeys]
+}
+
+export type PickByType<Obj extends object, Type> = {
+    [ObjKey in keyof Obj as Obj[ObjKey] extends never ? never : Type extends Obj[ObjKey] ? ObjKey : never]: Obj[ObjKey];
+};
+
+export type ObjectToUnion<Obj extends object> = {
+    [ObjKey in keyof Obj]: { [ObjKey2 in ObjKey]: Obj[ObjKey]; };
+}[keyof Obj];
